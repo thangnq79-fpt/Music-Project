c4ef316ccea26966b13cac3d693d3bb7a64c84b3
thangnq79-fpt <thangnqde150240@fpt.edu.vn>
2022-09-16 07:44:06 +0700

    complete playing music

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <app-header />
-  <router-view></router-view>
+  <router-view v-slot="{ Component }">
+    <transition name="fade" mode="out-in">
+      <component :is="Component"></component>
+    </transition>
+  </router-view>
   <player />
   <auth />
 </template>
@@ -30,3 +34,18 @@ export default {
   },
 };
 </script>
+<style>
+.fade-enter-from {
+  opacity: 0;
+}
+
+.fade-enter-active {
+  transition: all 0.5s linear;
+}
+
+.fade-leave-to {
+  transition: all 0s lienar;
+  opacity: 0;
+}
+</style>
+  
